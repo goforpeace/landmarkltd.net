97bea008ef06a2d4f10f81522f5f5936130e5356
Landmark New Homes LTD. <landmarknewhomesltd@gmail.com>
2025-11-28 23:18:14 +0000

    in browser tab it show Landmark Estates , make it Landmark New Homes Ltd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'Landmark Estates',
+  title: 'Landmark New Homes Ltd.',
   description: 'Your Dream Home Awaits',
 };
 
