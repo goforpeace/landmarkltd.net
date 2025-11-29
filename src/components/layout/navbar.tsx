ecbe6a62c412604b4f54dbaab5f2e7d05836618d
Landmark New Homes LTD. <landmarknewhomesltd@gmail.com>
2025-11-29 09:02:02 +0000

    make the nav bar color full white

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-gray-100">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-white">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="flex items-center gap-2" prefetch={false}>
           <Image
